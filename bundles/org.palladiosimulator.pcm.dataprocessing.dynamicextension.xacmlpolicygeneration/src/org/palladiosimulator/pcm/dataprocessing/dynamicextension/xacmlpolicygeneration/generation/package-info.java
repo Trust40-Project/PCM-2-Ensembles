cf6177096da97a06e1451e5a06147dbee9bfad7c
Lolalol97 <jojo.schenkenberger@web.de>
2019-08-20 17:44:20 +0200

    package-infos added and checkstyle done

---
+++ bundles/org.palladiosimulator.pcm.dataprocessing.dynamicextension.xacmlpolicygeneration/src/org/palladiosimulator/pcm/dataprocessing/dynamicextension/xacmlpolicygeneration/generation/package-info.java
@@ -0,0 +1,7 @@
+/**
+ * Package for the generation of the XACML policy set from the given PCM context model instance.
+ * 
+ * @author Jonathan Schenkenberger
+ * @version 1.0
+ */
+package org.palladiosimulator.pcm.dataprocessing.dynamicextension.xacmlpolicygeneration.generation;
